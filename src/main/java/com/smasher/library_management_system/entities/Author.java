b7468744f1a1b784cfb9ec2877a4b8b898a17190
Shubham Bhosale <bhosaleshubham2512@gmail.com>
2025-09-15 19:17:30 +0530

    Added test for Repository Using H2-DB

--- src/main/java/com/smasher/library_management_system/entities/Author.java
+++ src/main/java/com/smasher/library_management_system/entities/Author.java
@@ -1,10 +1,7 @@
 package com.smasher.library_management_system.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -13,6 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @Setter
 @Getter
+@Builder
 public class Author {
 
     @Id
---
+++ src/test/java/com/smasher/library_management_system/repositories/AuthorRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.smasher.library_management_system.repositories;
+
+import com.smasher.library_management_system.entities.Author;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AuthorRepositoryTest {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    private Author author;
+
+    @BeforeEach
+    void setUp() {
+         author = Author.builder()
+                .name("Shubham")
+                .email("shubham@gmail.com")
+                .build();
+    }
+
+    @Test
+    void testFindByEmail_whenEmailIsPresent_thenReturnAuthorList() {
+//        Arrange,  Given
+        authorRepository.save(author);
+
+//        Act,  When
+        List<Author> authors = authorRepository.findByEmail(author.getEmail()).orElse(null);
+
+//        Assert,  Then
+        assertThat(authors).isNotNull();
+        assertThat(authors).isNotEmpty();
+        assertThat(authors.get(0).getEmail()).isEqualTo(author.getEmail());
+    }
+
+    @Test
+    void testFindByEmail_whenEmailIsNotFound_thenReturnEmptyAuthorList() {
+//        Arrange
+        String email = "notPresent.123@gmail.com";
+
+//        Act
+        List<Author> authors = authorRepository.findByEmail(email).orElse(null);
+
+//        Assert
+        assertThat(authors).isNotNull();
+        assertThat(authors).isEmpty();
+
+    }
+}
